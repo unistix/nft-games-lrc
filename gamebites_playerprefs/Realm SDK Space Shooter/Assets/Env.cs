a53f62cc8c3bc8ce08f578ba1f113982020af00b
Cynny <c.dafinone@gmail.com>
2022-09-07 18:36:15 +0100

    env test

---
+++ gamebites_playerprefs/Realm SDK Space Shooter/Assets/Env.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Env
+{
+    public readonly string mongo_name;
+    public readonly string mongo_pass = "Cupcake1234";
+    public Env()
+    {
+        mongo_name = "unistix";
+        mongo_pass = "Cupcake1234";
+
+    }
+}
+    // Start is called before the first frame update
+    
