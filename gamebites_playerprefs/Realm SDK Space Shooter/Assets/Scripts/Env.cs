5a24293024e7513e7941fa9b90fc55a0617b9ae1
Cynny <c.dafinone@gmail.com>
2022-09-10 18:19:20 +0100

    update score function complete

---
+++ gamebites_playerprefs/Realm SDK Space Shooter/Assets/Scripts/Env.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Env
+{
+    public readonly string realm_name;
+    public readonly string realm_pass;
+    public readonly string appID;
+    public Env()
+    {
+        realm_name = "c.dafinone@gmail.com";
+        realm_pass = "Cupcake1234";
+        appID = "space-shooter-svqqq";
+
+    }
+}
+    // Start is called before the first frame update
+    
--- gamebites_playerprefs/Realm SDK Space Shooter/Assets/Scripts/RealmController.cs
+++ gamebites_playerprefs/Realm SDK Space Shooter/Assets/Scripts/RealmController.cs
@@ -25,7 +25,7 @@ public class RealmController : MonoBehaviour
         DontDestroyOnLoad(gameObject);
         Instance = this;
 
-        tstrintest = Login(env.realm_name, env.realm_pass);
+        tstrintest = Login(env.realm_name, env.realm_pass);//test Login eventually this will be a button 
     }
 
     void OnDisable()
@@ -36,11 +36,24 @@ public class RealmController : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        //Score Button Test
+
+        PlayerProfile _playerProfile = GetPlayerProfile();
+        if (_playerProfile != null)
+        {
+            
+                Debug.Log(_playerProfile.Score);
+            
+        }
+    }
+
     //Keep functions which effect DB within Realm controller
     //Don't create multiple realm controller scripts in this scene
     //Dont create specific functions for loading data since loading is now built in
 
-    
+
     public async Task<string> Login(string email, string password)
     {
         if (email != "" && password != "")
@@ -53,9 +66,10 @@ public class RealmController : MonoBehaviour
             {
                 if (_realmUser == null)
                 {
-                   //This is where the UI light error handling should take place
-                   //if use is null create a new user and update text > Creating New Account
-                    
+
+                    //This is where the UI light error handling should take NOT place
+                    //if use is null create a new user and update text > Creating New Account 
+
                     _realmUser = await _realmApp.LogInAsync(Credentials.EmailPassword(email, password));
                     _realm = await Realm.GetInstanceAsync(new PartitionSyncConfiguration(email, _realmUser));
                     Debug.Log(_realm.SyncSession.ConnectionState);
@@ -81,18 +95,52 @@ public class RealmController : MonoBehaviour
 
         return "";
 
-        /*
-        public PlayerProfile GetPlayerProfile() { }
-
-        public void IncreaseScore() { }
-
-        public void ResetScore() { }
-
-        public bool IsSparkBlasterEnabled() { }
-
-        public bool IsCrossBlasterEnabled() { }*/
+        
         
 
+
     }
+
+    public PlayerProfile GetPlayerProfile()
+    {
+        //This is where the UI light error handling should take NOT place - it will be called regulary
+        PlayerProfile _playerProfile = _realm.Find<PlayerProfile>(_realmUser.Id);
+
+        if (_playerProfile == null)
+        {
+
+           
+            _realm.Write(() => {
+                _playerProfile = _realm.Add(new PlayerProfile(_realmUser.Id));
+            });
+        }
+
+       
+        return _playerProfile;
+
+
+    }
+
+
+
+    public void IncreaseScore()
+    {
+        PlayerProfile _playerProfile = GetPlayerProfile();
+        if (_playerProfile != null)
+        {
+            _realm.Write(() =>
+            {
+                _playerProfile.Score++;
+            });
+        }
+    }
+    
+    /*
+
+    public void ResetScore() { }
+
+    public bool IsSparkBlasterEnabled() { }
+
+    public bool IsCrossBlasterEnabled() { }*/
 
 }
